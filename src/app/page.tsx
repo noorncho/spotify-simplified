9bb23ebd978892048018056a49bcc8e3f46e24bc
Noor Ncho <nrandin@gmail.com>
2023-05-13 16:32:47 +0100

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+//index.html
+
 import Image from 'next/image'
 import styles from './page.module.css'
 
@@ -49,7 +51,7 @@ export default function Home() {
           <h2>
             Docs <span>-&gt;</span>
           </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
+          <p>Find in-depth information about Next.js features and API. Testing testing 123</p>
         </a>
 
         <a
